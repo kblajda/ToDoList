81e443f45c923e975c62f389e21b8ca2f06831c2
kblajda <k.blajda1@gmail.com>
2019-12-20 17:53:12 +0100

    Create function to add&remove task from list

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,30 @@
-const person = {
-    name: "Krystian",
-    age: 25
-};
-console.log(person.name);
+const form = document.querySelector('.toDoForm')
+const taskList = document.querySelector('.toDoList__list');
+const taskNumber = document.querySelector('.toDoForm__counter span');
+const listItem = document.getElementsByClassName('toDoList__item');
+const inputTask = document.querySelector('.toDoForm__input')
+
+
+
+
+//function to add task on task list.
+const addTask = (e) => {
+    e.preventDefault();
+    const taskName = inputTask.value;
+    // console.log(taskName);
+    if (taskName === "") return;
+    const newTask = document.createElement('li');
+    newTask.className = 'toDoList__item';
+    newTask.innerHTML = taskName + "<button>Delete</button>";
+    taskList.appendChild(newTask);
+    inputTask.value = "";
+    taskNumber.textContent = listItem.length;
+    newTask.querySelector('button').addEventListener('click', removeTask);
+}
+
+// function to remove task from task list.
+const removeTask = (e) => {
+    e.target.parentNode.remove();
+    taskNumber.textContent = listItem.length;
+}
+form.addEventListener('submit', addTask);
