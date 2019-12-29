4e5fb1f927ac9c8b2b2a6f9f19042dcfb6296832
kblajda <k.blajda1@gmail.com>
2019-12-29 22:30:28 +0100

    Create Array to storage list elemnts
    Modify add&remove function to cooperate with array

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+const toDoList = [];
 const form = document.querySelector('.toDoForm')
 const taskList = document.querySelector('.toDoList__list');
 const taskNumber = document.querySelector('.toDoForm__counter span');
@@ -11,11 +12,12 @@ const inputTask = document.querySelector('.toDoForm__input')
 const addTask = (e) => {
     e.preventDefault();
     const taskName = inputTask.value;
-    // console.log(taskName);
     if (taskName === "") return;
     const newTask = document.createElement('li');
     newTask.className = 'toDoList__item';
     newTask.innerHTML = taskName + "<button>Delete</button>";
+    toDoList.push(newTask);
+    renderList();
     taskList.appendChild(newTask);
     inputTask.value = "";
     taskNumber.textContent = listItem.length;
@@ -24,7 +26,21 @@ const addTask = (e) => {
 
 // function to remove task from task list.
 const removeTask = (e) => {
-    e.target.parentNode.remove();
+    //remove from array
+    const index = e.target.parentNode.dataset.key; // take dataset remove elements form array
+    toDoList.splice(index);
     taskNumber.textContent = listItem.length;
+    renderList();
 }
+
+// function to render array elements, use in add & remove elements
+const renderList = () => {
+    taskList.textContent = "";
+    toDoList.forEach((toDoItem, key) => { // Add task to array, add dataset to array elements
+        toDoItem.dataset.key = key;
+        taskList.appendChild(toDoItem);
+    })
+}
+
+
 form.addEventListener('submit', addTask);
